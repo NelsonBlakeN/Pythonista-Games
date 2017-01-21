8a2ebc125e8b69c51f135b167e315938a67ff614
NelsonBlakeN <nelson.blaken@gmail.com>
2017-01-21 16:55:09 -0600

    Attempting to correct previous issue with git changes. Dropbox folder was not synching correctly.

--- Maze Run.py
+++ Maze Run.py
@@ -18,25 +18,26 @@ class Game (Scene):
 		#Center between the walls
 		#Walls are spawned an equal distance in both directions
 		#Will be shifted in the game
-
+		
 		time_font = ('Avenir Next Condensed', 32)
 		self.time_label = LabelNode('00:00', font=time_font, parent=self)
 		self.time_label.anchor_point = (0.5, 1)
 		self.time_label.position = (self.size.w/2, self.size.h-16)
 		self.time_label.z_position = 1
-
+		
 		self.highscore = self.load_highscore()
 		self.show_start_menu()
-
+		
 	def beg_scene(self):
 		y = 32
 		while y < self.size.h+64:
 			self.spawn_walls(y=y)
 			y += 64
-
+			
 	def new_game(self):
 		for w in self.walls:
 			w.remove_from_parent()
+		self.last_row = Node(parent=self)
 		self.wall_dist = 96
 		self.wall_center = self.size.w/2
 		self.timer = 0
@@ -47,14 +48,14 @@ class Game (Scene):
 		self.time_label.text = '00:00'
 		self.start_time = self.t
 		self.beg_scene()
-
+		
 	def load_highscore(self):
 		try:
 			with open('.MazeRun_highscore', 'r') as f:
 				return int(f.read())
 		except:
 			return 0
-
+			
 	def update(self):
 		self.move_ship()
 		self.update_walls()
@@ -62,57 +63,66 @@ class Game (Scene):
 		self.score = int(self.t - self.start_time)
 		sec_in = self.score
 		self.time_label.text = '%02d:%02d' % (sec_in/60, sec_in%60)
-
+		
 	def update_walls(self):
 		if self.timer % 64 == 0:
 			self.spawn_walls()
 			#Time to make a new row
-			if random.random() < 0.45:
+			if random.random() < 0.65:
 				#Turn is happening
 				if random.random() < 0.5 and self.can_turn(96):
 					self.wall_center += 32
 				elif random.random() > 0.5 and self.can_turn(-96):
 					self.wall_center -= 32
-
-				self.speed = min(15, self.speed+0.025)
-
+					
+				self.speed = min(15, self.speed+0.05)
+				
 		self.move_walls()
 		self.timer += 1
-
+		
 	def can_turn(self, dir):
 		if(self.wall_center+dir > 0 and self.wall_center+dir < self.size.w):
 			return True
 		return False
-
+		
 	def spawn_walls(self, y=None):
+		print("Last row position: ", self.last_row.position)
 		#Not allowed to use 'self' in args
 		if y == None:
 			y = self.size.h+72
-
-	 	# Making changes/..
-
-		ledge = SpriteNode(xWall, position=(self.wall_center-self.wall_dist, y), parent=self)
-		redge = SpriteNode(xWall, position=(self.wall_center+self.wall_dist, y), parent=self)
+			
+		new_row = Node(position=(0, y), parent=self)
+			
+		ledge = SpriteNode(xWall, position=(self.wall_center-self.wall_dist, y))
+		redge = SpriteNode(xWall, position=(self.wall_center+self.wall_dist, y))
 		ledge.rotation=-math.pi/2
 		redge.rotation=math.pi/2
+		new_row.add_child(ledge)
+		new_row.add_child(redge)
 		self.walls.append(ledge)
 		self.walls.append(redge)
-
+		
+		self.last_row = new_row
+		print("New row loc: ", new_row.position)
+		print("Last row: ", self.last_row)
+		
 		t = 1
 		while t <= 3:
 			lx = self.wall_center-self.wall_dist-(64*t)
 			rx = self.wall_center+self.wall_dist+(64*t)
-			ltile = SpriteNode(iWall, position=(lx, y), parent=self)
-			rtile = SpriteNode(iWall, position=(rx, y), parent=self)
-
+			ltile = SpriteNode(iWall, position=(lx, y))
+			rtile = SpriteNode(iWall, position=(rx, y))
+			
 			ltile.rotation=-math.pi/2
 			rtile.rotation=math.pi/2
-
+			
+			new_row.add_child(ltile)
+			new_row.add_child(rtile)
 			self.walls.append(ltile)
 			self.walls.append(rtile)
-
+			
 			t+=1
-
+			
 	def move_walls(self):
 		dy = -1*self.speed
 		for tile in self.walls:
@@ -120,29 +130,28 @@ class Game (Scene):
 			if tile.position.y < -32:
 				tile.remove_from_parent()
 				self.walls.remove(tile)
-
+				
 	def check_wall_collision(self):
 		size_x = self.player.size[0]
 		size_y = self.player.size[1]
 		player_hitbox = Rect(self.player.position.x - size_x/2, 128, size_x, size_y)
-
+		
 		for wall in list(self.walls):
 			if wall.frame.intersects(player_hitbox):
-				print("Crashed")
 				self.player_crash(self.player)
-
+				
 	def player_crash(self, p):
 		sound.play_effect('digital:ZapThreeToneDown')
 		p.remove_from_parent()
 		self.end_game()
-
+		
 	def spawn_player(self):
 		self.player = SpriteNode('spc:PlayerLife1Blue', parent=self)
 		self.player.anchor_point = (0.5, 0)
 		self.player.position = (self.size.w/2, 128)
 		self.player_target = self.size.w/2
 		self.player.z_position = 1
-
+		
 	def end_game(self):
 		if self.score > self.highscore:
 			self.highscore = self.score
@@ -150,37 +159,38 @@ class Game (Scene):
 		self.paused = True
 		self.menu = MenuScene('Game Over', 'Highscore: %02d:%02d' % (self.highscore/60, self.highscore%60), ['New Game'])
 		self.present_modal_scene(self.menu)
-
+		
 	def save_highscore(self):
 		with open('.MazeRun_highscore', 'w') as f:
 			f.write(str(self.highscore))
-
+			
 	def show_start_menu(self):
 		self.paused = True
 		self.menu = MenuScene('Maze Run', 'Highscore: %02d:%02d' % (self.highscore/60, self.highscore%60), ['New Game'])
 		self.present_modal_scene(self.menu)
-
+		
 	def menu_button_selected(self, title):
 		if title in ('New Game', 'Play'):
 			self.dismiss_modal_scene()
 			self.menu = None
 			self.paused = False
 			self.new_game()
-
+			
 	def move_ship(self):
 		dx = self.player_target - self.player.position.x
 		self.player.position += dx, 0
-
+		
 	def touch_began(self, touch):
 		x, y = touch.location
 		self.player_target = x
-
+		
 	def touch_moved(self, touch):
 		x, y = touch.location
 		self.player_target = x
-
+		
 	def touch_ended(self, touch):
 		pass
-
+		
 if __name__ == '__main__':
 	run(Game(), show_fps=True)
+
